4d0087e8ff76d2d7afdf6705c58ea1422f96f54a
osama <oaj09@ic.ac.uk>
2012-01-31 19:49:44 +0000

    first exercise finished

--- Practical 2.c
+++ Practical 2.c
@@ -1,31 +1,50 @@
 const int rightWheel = motorA;
 const int leftWheel = motorB;
-int ForwardSpeed = 10;
-int StraightTime  =835;
-int TurnSpeed = 10;
-int TurnTime = 171;
+int ForwardSpeed ;
+int StraightTime ;
+int timeUnit ;
+int TurnSpeed;
+int TurnTime ;
+int x;
+int y;
+int angle;
 
 void Forward40cm(){
   motor[rightWheel] = ForwardSpeed;
   motor[leftWheel] = ForwardSpeed;
-  wait10Msec(StraightTime);
+  int i;
+  for (i = 0; i < StraightTime; i += timeUnit) {
+    nxtSetPixel( x , y );
+    x += cosDegrees(angle);
+    y += sinDegrees(angle);
+    wait1Msec(timeUnit);
+  }
 }
 
 void Backward40cm(){
   motor[rightWheel] = ForwardSpeed*-1;
   motor[leftWheel] = ForwardSpeed*-1;
-  wait10Msec(StraightTime);
+  int i;
+  for ( i = 0 ; i < StraightTime ; i+=timeUnit) {
+    nxtSetPixel( x , y );
+    x -= cosDegrees(angle);
+    y -= sinDegrees(angle);
+    //wait1Msec(timeUnit);
+  }
 }
 
 void Left90deg(){
   motor[rightWheel] = TurnSpeed;
   motor[leftWheel] = TurnSpeed*-1;
+  angle = (angle+90) % 360;
+
   wait10Msec(TurnTime);
 }
 
 void Right90deg(){
   motor[rightWheel] = TurnSpeed*-1;
   motor[leftWheel] = TurnSpeed;
+  angle = (angle-90 )% 360;
   wait10Msec(TurnTime);
 }
 
@@ -35,8 +54,19 @@ void chill(){
   wait10Msec(100);
 }
 
+ void init(){
+   ForwardSpeed = 10;
+   StraightTime  =8360;
+   timeUnit = 209;
+   TurnSpeed = 10;
+   TurnTime = 171;
+   x = 15;
+   y = 15;
+   angle = 0;
+ }
 task main(){
   //Right90deg();
+  init();
   wait10Msec(100);
   Forward40cm();
   chill();
@@ -53,4 +83,7 @@ task main(){
   Forward40cm();
   chill();
   Left90deg();
+  motor[rightWheel] = 0;
+  motor[leftWheel] = 0;
+  wait10Msec(500);
 }
